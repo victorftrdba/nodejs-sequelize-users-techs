f837cd3665b8651ad5d98fb454f18f897e1d1375
Victor Nogueira <victornogu80@gmail.com>
2022-01-03 14:13:14 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    host: 'localhost',
+    dialect: 'mysql',
+    database: 'nodejs-users-techs',
+    username: 'dev',
+    password: 'dev102030',
+    define: {
+        timestamps: true,
+        underscored: true,
+    }
+};
